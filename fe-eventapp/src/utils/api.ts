5b7447b43e9b23e814739aa97a2366d776482e0c
annc4st <annaseniuta@gmail.com>
2026-04-19 11:38:11 +0100

    cleaned up api.ts

--- fe-eventapp/src/utils/api.ts
+++ fe-eventapp/src/utils/api.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// import store from '../store/store';
 
 const api = axios.create({
   baseURL: 'http://localhost:9001',
@@ -8,17 +7,6 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// api.interceptors.request.use((config) => {
-//   const state = store.getState();
-//   const token = state.user.token;
-
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-
-//   return config;
-// });
 
  
  export default api;
